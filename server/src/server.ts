86dd7cb02c15b722b2e7c5511f3e55b702a48a79
Giovanni Lopes <giovanniclopes@gmail.com>
2023-01-18 18:00:17 -0300

    'prisma' and '@prisma/client' added

--- server/src/server.ts
+++ server/src/server.ts
@@ -1,1 +1,17 @@
-console.log("Hello World!")
+import Fastify from "fastify";
+
+const app = Fastify()
+
+/* 
+* Método HTTP: Get, Post, Put, Patch, Delete
+*/
+
+app.get('/', () => {
+  return 'Hello NLW'
+})
+
+app.listen({
+  port: 3333
+}).then(() => {
+  console.log('HTTP Server Running')
+})
