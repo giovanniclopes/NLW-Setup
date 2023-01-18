d589daccfb8c06738fe1bd85f9cde7a73a912656
Giovanni Lopes <giovanniclopes@gmail.com>
2023-01-18 18:51:32 -0300

    'tailwindcss' added

--- web/src/components/Habit.tsx
+++ web/src/components/Habit.tsx
@@ -1,9 +1,11 @@
 interface HabitProps {
-  completed: number
+  completed: number;
 }
 
 export function Habit(props: HabitProps) {
   return (
-    <p>{props.completed}</p>
-  )
+    <div className="bg-zinc-900 w-10 text-white rounded m-2 flex items-center justify-center">
+      {props.completed}
+    </div>
+  );
 }
