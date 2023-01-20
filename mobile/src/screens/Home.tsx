7b8f32a4670aebd798ee33caa2ab5fe6ec457bf1
Giovanni Lopes <giovanniclopes@gmail.com>
2023-01-19 22:08:50 -0300

    'day' intervals created

--- mobile/src/screens/Home.tsx
+++ mobile/src/screens/Home.tsx
@@ -1,9 +1,14 @@
-import { View, Text } from "react-native";
+import { View, Text, ScrollView } from "react-native";
 
-const weekDays = ['D', 'S', 'T', 'Q', 'Q', 'S', 'S'];
+import { generateRangeDatesFromYearStart } from "../utils/generate-range-between-dates";
 
 import { HabitDay, DAY_SIZE } from "../components/HabitDay";
 import { Header } from "../components/Header";
+
+const weekDays = ["D", "S", "T", "Q", "Q", "S", "S"];
+const datesFromYearStart = generateRangeDatesFromYearStart();
+const minimumSSummaryDatesSize = 18 * 5;
+const amountOfDaysToFill = minimumSSummaryDatesSize - datesFromYearStart.length;
 
 export function Home() {
   return (
@@ -11,20 +16,34 @@ export function Home() {
       <Header />
 
       <View className="flex-row mt-6 mb-2">
-        {
-          weekDays.map((weekDay, i) => (
-            <Text 
-              key={`${weekDay}-${i}`}
-              className="text-zinc-400 text-xl font-bold text-center m-1"
-              style={{ width: DAY_SIZE}}
-            >
-              {weekDay}
-            </Text>
-          ))
-        }
+        {weekDays.map((weekDay, i) => (
+          <Text
+            key={`${weekDay}-${i}`}
+            className="text-zinc-400 text-xl font-bold text-center m-1"
+            style={{ width: DAY_SIZE }}
+          >
+            {weekDay}
+          </Text>
+        ))}
       </View>
 
-      <HabitDay />
+      <ScrollView
+        showsVerticalScrollIndicator={false}
+        contentContainerStyle={{ paddingBottom: 100 }}
+      >
+        <View className="flex-row flex-wrap">
+          {datesFromYearStart.map((date) => (
+            <HabitDay key={date.toISOString()} />
+          ))}
+          {amountOfDaysToFill > 0 &&
+            Array.from({ length: amountOfDaysToFill }).map((_, index) => (
+              <View
+                className="bg-zinc-900 rounded-lg border-2 border-zinc-800 m-1 opacity-40"
+                style={{ width: DAY_SIZE, height: DAY_SIZE }}
+              />
+            ))}
+        </View>
+      </ScrollView>
     </View>
-  )
+  );
 }
