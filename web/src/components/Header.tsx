3d2484e2db35f343a80f2e9ec5f083b673bd4429
Giovanni Lopes <giovanniclopes@gmail.com>
2023-01-20 19:36:33 -0300

    'radix-ui' added

--- web/src/components/Header.tsx
+++ web/src/components/Header.tsx
@@ -1,18 +1,31 @@
 import { Plus } from "phosphor-react";
+import * as Dialog from "@radix-ui/react-dialog";
+import { useState } from "react";
+
 import logoImage from "../assets/logo.svg";
 
 export function Header() {
   return (
     <div className="w-full max-w-3xl mx-auto flex items-center justify-between">
-      <img src={logoImage} alt="Habits" />
+      <img src={logoImage} alt="Habits" draggable={false} />
 
-      <button
-        type="button"
-        className="border border-violet-500 font-semibold rounded-lg px-6 py-4 flex items-center gap-3 trnasition-all hover:border-violet-300"
-      >
-        <Plus size={20} className="text-violet-500" />
-        Novo hábito
-      </button>
+      <Dialog.Root>
+        <Dialog.Trigger
+          type="button"
+          className="border border-violet-500 font-semibold rounded-lg px-6 py-4 flex items-center gap-3 trnasition-all hover:border-violet-300"
+        >
+          <Plus size={20} className="text-violet-500" />
+          Novo hábito
+        </Dialog.Trigger>
+        
+        <Dialog.Portal>
+          <Dialog.Overlay className="w-screen h-screen bg-black/80 fixed inset-0" />
+          <Dialog.Content className="absolute p-10 bg-zinc-900 rounded-2xl w-full max-w-md top-1/2 left-1/2">
+            Conteúdo do modal
+          </Dialog.Content>
+        </Dialog.Portal>
+      </Dialog.Root>
+
     </div>
   );
 }
