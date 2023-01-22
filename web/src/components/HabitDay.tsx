20a2b21a3c2bf31770c077d256fa2069cb8384d5
Giovanni Lopes <giovanniclopes@gmail.com>
2023-01-21 23:24:01 -0300

    'create new habit' created

--- web/src/components/HabitDay.tsx
+++ web/src/components/HabitDay.tsx
@@ -3,6 +3,7 @@ import * as Checkbox from "@radix-ui/react-checkbox";
 import clsx from "clsx";
 import { ProgressBar } from "./ProgressBar";
 import { Check } from "phosphor-react";
+import dayjs from "dayjs";
 
 interface HabitDayProps {
   date: Date;
@@ -10,35 +11,35 @@ interface HabitDayProps {
   amount?: number;
 }
 
-export function HabitDay({ completed = 0, amount = 0 }: HabitDayProps) {
+export function HabitDay({ completed = 0, amount = 0, date }: HabitDayProps) {
   const completedPorcentage =
     amount > 0 ? Math.round((completed / amount) * 100) : 0;
+
+  const dayAndMonth = dayjs(date).format('DD/MM')
+  const dayOfWeek = dayjs(date).format('dddd')
 
   return (
     <Popover.Root>
       <Popover.Trigger
-        className={clsx(
-          "w-10 h-10 bg-zinc-900 border-2 border-zinc-800 rounded-lg",
-          {
-            "bg-zinc-900 border-zinc-800": completedPorcentage === 20,
-            "bg-violet-900 border-violet-700":
-              completedPorcentage >= 0 && completedPorcentage < 20,
-            "bg-violet-800 border-violet-600":
-              completedPorcentage >= 20 && completedPorcentage < 40,
-            "bg-violet-700 border-violet-500":
-              completedPorcentage >= 40 && completedPorcentage < 60,
-            "bg-violet-600 border-violet-500":
-              completedPorcentage >= 60 && completedPorcentage < 80,
-            "bg-violet-500 border-violet-400": completedPorcentage >= 80,
-          }
-        )}
+        className={clsx("w-10 h-10 bg-zinc-900 border-2 rounded-lg", {
+          "bg-zinc-900 border-zinc-800": completedPorcentage === 0,
+          "bg-violet-900 border-violet-700":
+            completedPorcentage > 0 && completedPorcentage < 20,
+          "bg-violet-800 border-violet-600":
+            completedPorcentage >= 20 && completedPorcentage < 40,
+          "bg-violet-700 border-violet-500":
+            completedPorcentage >= 40 && completedPorcentage < 60,
+          "bg-violet-600 border-violet-500":
+            completedPorcentage >= 60 && completedPorcentage < 80,
+          "bg-violet-500 border-violet-400": completedPorcentage >= 80,
+        })}
       />
 
       <Popover.Portal>
         <Popover.Content className="min-w-[328px] p-6 rounded-2xl bg-zinc-900 flex flex-col">
-          <span className="font-semibold text-zinc-400">Terça-feira</span>
+          <span className="font-semibold text-zinc-400">{dayOfWeek}</span>
           <span className="mt-1 font-extrabold leading-tight text-2xl">
-            17/01
+            {dayAndMonth}
           </span>
           <ProgressBar progress={completedPorcentage} />
 
