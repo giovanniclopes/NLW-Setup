4477410be0d31250ed7d8051f10610fd7c90d076
Giovanni Lopes <giovanniclopes@gmail.com>
2023-01-24 19:08:13 -0300

    'past dates' now has a disabled button

--- web/src/components/HabitsList.tsx
+++ web/src/components/HabitsList.tsx
@@ -3,6 +3,7 @@ import * as Checkbox from "@radix-ui/react-checkbox";
 import { Check } from "phosphor-react";
 import { useEffect, useState } from "react";
 import { api } from "./../lib/axios";
+import dayjs from "dayjs";
 
 interface HabitsList {
   date: Date;
@@ -31,11 +32,20 @@ export function HabitsList({ date }: HabitsList) {
         setHabitsInfo(response.data);
       });
   }, []);
+
+  const isDateInPast = dayjs(date)
+    .endOf("day")
+    .isBefore(new Date());
   return (
     <div className="mt-6 flex flex-col gap-3">
       {habitsInfo?.possibleHabits.map((habit) => {
         return (
-          <Checkbox.Root key={habit.id} checked={habitsInfo.completedHabits.includes(habit.id)} className="flex items-center gap-3 group">
+          <Checkbox.Root
+            key={habit.id}
+            checked={habitsInfo.completedHabits.includes(habit.id)}
+            disabled={isDateInPast}
+            className="flex items-center gap-3 group"
+          >
             <div className="h-8 w-8 rounded-lg flex items-center justify-center bg-zinc-900 border-2 border-zinc-400 group-data-[state=checked]:bg-green-500 group-data-[state=checked]:border-green-500">
               <Checkbox.Indicator>
                 <Check size={20} className="text-white" />
