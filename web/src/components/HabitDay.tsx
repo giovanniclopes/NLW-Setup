36d9637dd4a595621fbad73f67e5218ec26b3fb0
Giovanni Lopes <giovanniclopes@gmail.com>
2023-01-24 19:59:27 -0300

    'web' completed

--- web/src/components/HabitDay.tsx
+++ web/src/components/HabitDay.tsx
@@ -27,18 +27,21 @@ export function HabitDay({ defaultCompleted = 0, amount = 0, date }: HabitDayPro
   return (
     <Popover.Root>
       <Popover.Trigger
-        className={clsx("w-10 h-10 bg-zinc-900 border-2 rounded-lg", {
-          "bg-zinc-900 border-zinc-800": completedPorcentage === 0,
-          "bg-violet-900 border-violet-700":
-            completedPorcentage > 0 && completedPorcentage < 20,
-          "bg-violet-800 border-violet-600":
-            completedPorcentage >= 20 && completedPorcentage < 40,
-          "bg-violet-700 border-violet-500":
-            completedPorcentage >= 40 && completedPorcentage < 60,
-          "bg-violet-600 border-violet-500":
-            completedPorcentage >= 60 && completedPorcentage < 80,
-          "bg-violet-500 border-violet-400": completedPorcentage >= 80,
-        })}
+        className={clsx(
+          "w-10 h-10 bg-zinc-900 border-2 rounded-lg transition-colors focus:outline-none focus:ring-2 focus:ring-violet-600 focus:ring-offset-2 focus:ring-offset-background",
+          {
+            "bg-zinc-900 border-zinc-800": completedPorcentage === 0,
+            "bg-violet-900 border-violet-700":
+              completedPorcentage > 0 && completedPorcentage < 20,
+            "bg-violet-800 border-violet-600":
+              completedPorcentage >= 20 && completedPorcentage < 40,
+            "bg-violet-700 border-violet-500":
+              completedPorcentage >= 40 && completedPorcentage < 60,
+            "bg-violet-600 border-violet-500":
+              completedPorcentage >= 60 && completedPorcentage < 80,
+            "bg-violet-500 border-violet-400": completedPorcentage >= 80,
+          }
+        )}
       />
 
       <Popover.Portal>
